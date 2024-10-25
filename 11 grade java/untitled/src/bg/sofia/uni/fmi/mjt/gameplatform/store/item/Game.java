683e277c9c76ec1f9f3da973030653e28acb02ba
Monika Georgieva <monikageorgieva950@gmail.com>
2024-10-25 13:56:22 +0300

    update

--- 11 grade java/untitled/src/bg/sofia/uni/fmi/mjt/gameplatform/store/item/Game.java
+++ 11 grade java/untitled/src/bg/sofia/uni/fmi/mjt/gameplatform/store/item/Game.java
@@ -1,4 +1,65 @@
 package bg.sofia.uni.fmi.mjt.gameplatform.store.item;
 
-public class Game {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game implements StoreItem {
+    private String title;
+    private BigDecimal price;
+    private LocalDateTime releaseDate;
+    private String description;
+    private List<Double> rating;
+
+    public Game(String title, BigDecimal price, LocalDateTime releaseDate, String genre) {
+        this.title = title;
+        this.price = price;
+        this.releaseDate = releaseDate;
+        this.description = genre;
+        this.rating = new ArrayList<>();
+    }
+
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+
+    @Override
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public double getRating() {
+//        for (Double rating : this.rating) {
+//            sum += rating;
+//        }
+        return rating.stream().reduce(0d, Double::sum) / rating.size();
+    }
+
+    @Override
+    public LocalDateTime getReleaseDate() {
+        return this.releaseDate;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public void setReleaseDate(LocalDateTime releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public void rate(double rating) {
+        this.rating.add(rating);
+    }
 }
--- 11 grade java/untitled/src/bg/sofia/uni/fmi/mjt/gameplatform/store/item/GameBundle.java
+++ 11 grade java/untitled/src/bg/sofia/uni/fmi/mjt/gameplatform/store/item/GameBundle.java
@@ -1,4 +1,15 @@
 package bg.sofia.uni.fmi.mjt.gameplatform.store.item;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class GameBundle {
+    private String title;
+    private BigDecimal price;
+    private LocalDateTime releaseDate;
+
+    public GameBundle(String title, BigDecimal price, LocalDateTime releaseDate, Game[] games)
+    {
+        
+    }
 }
